c675a102fbdcd7d6959291f513a923212c3bd5fe
Vatsal Narula <vatsalnarula@Vatsals-MacBook-Pro.local>
2023-06-13 21:14:01 -0400

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import logo from './logo.svg';
+
+const App = () => {
+
+  return (
+    <div className="App">
+      <button className="btn">
+        <Login></Login>
+       
+      </button>
+      <button className="btn">
+        Log Out
+      </button>
+    </div>
+  );
+};
+
+export default App;
+
+
